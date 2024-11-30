7dcab4384140ca78bd4b58ede5e22d0bae3e61f0
Gabriel Francois <gfrancois2003@gmail.com>
2024-11-30 02:00:49 +0100

    files: init bonus files

---
+++ get_next_line_bonus.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 01:54:41 by gfrancoi          #+#    #+#             */
+/*   Updated: 2024/11/30 01:57:14 by gfrancoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	ft_strjoin(char **s1, char *s2)
+{
+	size_t	i;
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*join;
+
+	s1_len = ft_strlen_char(*s1, 0);
+	s2_len = ft_strlen_char(s2, 0);
+	join = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!join)
+		return ((void) 0);
+	i = 0;
+	while (*s1 && (*s1)[i])
+	{
+		join[i] = (*s1)[i];
+		i++;
+	}
+	while (s2[i - s1_len])
+	{
+		join[i] = s2[i - s1_len];
+		i++;
+	}
+	join[i] = 0;
+	free(*s1);
+	*s1 = join;
+}
+
+void	ft_strcut(char **to_cut, char **paste)
+{
+	size_t	len;
+	size_t	to_cut_len;
+	char	*temp;
+
+	if (!(*to_cut))
+		return ((void)0);
+	temp = *to_cut;
+	len = ft_strlen_char(temp, '\n');
+	if (ft_strrchr(temp, '\n') != NULL)
+		len++;
+	if (*paste)
+	{
+		free(*paste);
+		*paste = 0;
+	}
+	*paste = ft_substr(temp, 0, len);
+	if (!paste)
+		return ((void)0);
+	to_cut_len = ft_strlen_char(temp, 0);
+	*to_cut = ft_substr(temp + len, 0, to_cut_len);
+	free(temp);
+}
+
+int	buffer_alloc(char **buffer)
+{
+	if (*buffer == NULL)
+		*buffer = ft_calloc(1, sizeof(char));
+	if (*buffer == NULL)
+		return (0);
+	return (1);
+}
+
+void	read_file(int fd, char **buffer)
+{
+	char	*content;
+	int		nb_read;
+
+	if (!buffer_alloc(buffer))
+		return ((void)0);
+	if (ft_strrchr(*buffer, '\n') != NULL)
+		return ((void)0);
+	content = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	while (1)
+	{
+		nb_read = read(fd, content, BUFFER_SIZE);
+		content[nb_read] = 0;
+		if (nb_read <= 0)
+		{
+			if (ft_strlen_char(*buffer, 0) == 0)
+				return (free(*buffer), *buffer = NULL, free(content));
+			break ;
+		}
+		ft_strjoin(buffer, content);
+		if (nb_read < BUFFER_SIZE || ft_strrchr(*buffer, '\n') != NULL)
+			break ;
+	}
+	free(content);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (read(fd, 0, 0) < 0)
+	{
+		if (buffer)
+			free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	read_file(fd, &buffer);
+	if (!buffer)
+		return (NULL);
+	line = NULL;
+	ft_strcut(&buffer, &line);
+	if (!line)
+		free(buffer);
+	return (line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 01:55:08 by gfrancoi          #+#    #+#             */
+/*   Updated: 2024/11/30 01:57:43 by gfrancoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*new_calloc;
+	size_t	calloc_len;
+
+	calloc_len = nmemb * size;
+	if (calloc_len < nmemb && calloc_len < size)
+		return (0);
+	new_calloc = malloc(calloc_len);
+	if (!new_calloc)
+		return (0);
+	while (calloc_len-- > 0)
+		new_calloc[calloc_len] = 0;
+	return (new_calloc);
+}
+
+size_t	ft_strlen_char(char const *str, char stop)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != stop)
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	size_t	length;
+	char	*dup;
+
+	length = ft_strlen_char(s, '\n');
+	dup = malloc(sizeof(char) * (length + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = 0;
+	return (dup);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = ft_strlen_char(s, 0);
+	while (s[i] != (unsigned char)c)
+	{
+		if (i == 0 && s[i] != c)
+			return (NULL);
+		i--;
+	}
+	return ((char *) &s[i]);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	sub_len;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	sub_len = 0;
+	while (start < ft_strlen_char(s, 0) && sub_len < len && s[start + sub_len])
+		sub_len++;
+	sub = ft_calloc(sub_len + 1, sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < sub_len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = 0;
+	return (sub);
+}
